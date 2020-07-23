dc57df891124fcc3a7133f8ae17a4eaca1f0120c
Chris Doty <dotyc@civicinc.com>
2020-07-22 20:53:34 -0500

    chore: ready to test

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const { exec } = require('child_process');
+const core = require('@actions/core');
+
+let version = core.getInput('version') || '0';
+if(version==="0")  {
+  console.log('version is required parameter')
+  process.exit(1);
+  return;
+}
+
+if(version.indexOf('-')>0) {
+  version=version.split('-')[0];
+}
 
 exec('git config --global user.name \'Chris Doty\'');
 exec('git config --global user.email \'cdotyone@users.noreply.github.com\'');
-exec('git commit -am "chore: commit v${{ steps.semver.outputs.new_version }} [ci skip]');
+exec(`git commit -am "chore: commit v${version} [ci skip]`);
+exec('git push origin:main --tags');
